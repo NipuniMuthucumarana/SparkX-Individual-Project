b10414dcfba48a6ef0e7ae4c9e18ced6a210c258
Nipuni <nipunimuthucumarana@gmail.com>
2020-08-30 22:19:33 +0530

    Patient Registration

---
+++ src/main/java/lk/sparkx/ncms/controller/PatientServlet.java
@@ -0,0 +1,100 @@
+package lk.sparkx.ncms.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Date;
+
+import lk.sparkx.ncms.models.Patient;
+import lk.sparkx.ncms.dao.PatientDao;
+
+@WebServlet(name = "PatientServlet")
+public class PatientServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String district = request.getParameter("district");
+        int locationX = Integer.parseInt(request.getParameter("locationX"));
+        int locationY = Integer.parseInt(request.getParameter("locationY"));
+        String severityLevel = request.getParameter("severityLevel");
+        String gender = request.getParameter("gender");
+        String contact = request.getParameter("contact");
+        String email = request.getParameter("email");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        java.util.Date dateAdmit = new java.util.Date();
+        Date admitDate = new Date(dateAdmit.getTime());
+
+        String admittedBy = request.getParameter("admittedBy");
+
+        java.util.Date dateDischarged = new java.util.Date();
+        Date dischargeDate = new Date(dateDischarged.getTime());
+
+        /*String paramDischarge = request.getParameter("admitDate");
+        Date dischargeDate = null;
+        try {
+            dischargeDate = (Date) date.parse(paramDischarge);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }*/
+        String dischargedBy = request.getParameter("dischargedBy");
+
+        
+        Patient patient = new Patient();
+        patient.setId(id);
+        patient.setFirstName(firstName);
+        patient.setLastName(lastName);
+        patient.setDistrict(district);
+        patient.setLocationX(locationX);
+        patient.setLocationY(locationY);
+        patient.setSeverityLevel(severityLevel);
+        patient.setGender(gender);
+        patient.setContact(contact);
+        patient.setEmail(email);
+        patient.setAge(age);
+        patient.setAdmitDate(admitDate);
+        patient.setAdmittedBy(admittedBy);
+        patient.setDischargeDate(dischargeDate);
+        patient.setDischargedBy(dischargedBy);
+
+        PatientDao patientDao = new PatientDao();
+        String patientRegistered = patientDao.registerPatient(patient);
+
+        if(patientRegistered.equals("SUCCESS"))   //On success, you can display a message to user on Home page
+        {
+            System.out.println("Success");
+            //request.getRequestDispatcher("/PatientDetails.jsp").forward(request, response);
+        }
+        else   //On Failure, display a meaningful message to the User.
+        {
+            System.out.println("Failed");
+            //request.setAttribute("errMessage", patientRegistered);
+            //request.getRequestDispatcher("/PatientRegister.jsp").forward(request, response);
+        }
+
+        try {
+            patientDao.registerPatient(patient);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Patient patient = new Patient();
+        patient.loadModel();
+        System.out.println("doGet success");
+        //response.getWriter().append("Served at: ").append(request.getContextPath());
+        //RequestDispatcher dispatcher = request.getRequestDispatcher("../web/PatientRegister.jsp");
+        //dispatcher.forward(request, response);
+    }
+
+}
---
+++ src/main/java/lk/sparkx/ncms/models/Patient.java
@@ -0,0 +1,204 @@
+package lk.sparkx.ncms.models;
+
+import com.google.gson.JsonObject;
+import lk.sparkx.ncms.dao.DBConnectionPool;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Date;
+import java.io.Serializable;
+
+public class Patient implements Serializable {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String district;
+    private int locationX;
+    private int locationY;
+    private String severityLevel;
+    private String gender;
+    private String contact;
+    private String email;
+    private int age;
+    private Date admitDate;
+    private String admittedBy;
+    private Date dischargeDate;
+    private String dischargedBy;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getLocationX() {
+        return locationX;
+    }
+
+    public void setLocationX(int locationX) {
+        this.locationX = locationX;
+    }
+
+    public int getLocationY() {
+        return locationY;
+    }
+
+    public void setLocationY(int locationY) {
+        this.locationY = locationY;
+    }
+
+    public String getSeverityLevel() {
+        return severityLevel;
+    }
+
+    public void setSeverityLevel(String severityLevel) {
+        this.severityLevel = severityLevel;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getAdmitDate() {
+        return admitDate;
+    }
+
+    public void setAdmitDate(Date admitDate) {
+        this.admitDate = admitDate;
+    }
+
+    public String getAdmittedBy() {
+        return admittedBy;
+    }
+
+    public void setAdmittedBy(String admittedBy) {
+        this.admittedBy = admittedBy;
+    }
+
+    public Date getDischargeDate() {
+        return dischargeDate;
+    }
+
+    public void setDischargeDate(Date dischargeDate) {
+        this.dischargeDate = dischargeDate;
+    }
+
+    public String getDischargedBy() {
+        return dischargedBy;
+    }
+
+    public void setDischargedBy(String dischargedBy) {
+        this.dischargedBy = dischargedBy;
+    }
+
+    public JsonObject serialize() {
+        JsonObject dataObject = new JsonObject();
+
+        dataObject.addProperty("id", this.id);
+        dataObject.addProperty("firstName", this.firstName);
+        dataObject.addProperty("lastName", this.lastName);
+        dataObject.addProperty("district", this.district);
+        dataObject.addProperty("locationX", this.locationX);
+        dataObject.addProperty("locationY", this.locationY);
+        dataObject.addProperty("district", this.district);
+        dataObject.addProperty("severityLevel", this.severityLevel);
+        dataObject.addProperty("gender", this.gender);
+        dataObject.addProperty("contact", this.contact);
+        dataObject.addProperty("email", this.email);
+        dataObject.addProperty("age", this.age);
+        dataObject.addProperty("admitDate", this.admitDate != null ? this.admitDate.toString() : null);
+        dataObject.addProperty("admittedBy", this.admittedBy);
+        dataObject.addProperty("dischargeDate", this.dischargeDate != null ? this.dischargeDate.toString() : null);
+        dataObject.addProperty("dischargedBy", this.dischargedBy);
+
+        return dataObject;
+    }
+
+    public void loadModel() {
+        try {
+            Connection connection = DBConnectionPool.getInstance().getConnection();
+            PreparedStatement statement;
+            ResultSet resultSet;
+
+            statement = connection.prepareStatement("SELECT * FROM patients WHERE id=? LIMIT 1");
+            statement.setInt(1, this.id);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                this.id = resultSet.getInt("id");
+                this.firstName = resultSet.getString("first_name");
+                this.lastName = resultSet.getString("last_name");
+                this.district = resultSet.getString("district");
+                this.locationX = resultSet.getInt("location_x");
+                this.locationY = resultSet.getInt("location_y");
+                this.severityLevel = resultSet.getString("severity_level");
+                this.gender = resultSet.getString("gender");
+                this.contact = resultSet.getString("contact");
+                this.email = resultSet.getString("email");
+                this.age = resultSet.getInt("age");
+                this.admitDate = resultSet.getDate("admit_date");
+                this.admittedBy = resultSet.getString("admitted_by");
+                this.dischargeDate = resultSet.getDate("discharge_date");
+                this.dischargedBy = resultSet.getString("discharged_by");
+            }
+
+            connection.close();
+        } catch (Exception exception) {
+
+        }
+    }
+}
